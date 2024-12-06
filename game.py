11ce05d054cf5fdaf67dfec496b4c95fac6d78b2
Maha <75489692+mahaqj@users.noreply.github.com>
2024-12-06 21:55:51 +0500

    update
    locking a path mechanism added, sprites can't go on that block in their own move_sprite func, as well as other fixes and game layout changes

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 import pygame
 from menu import *
+import time
+import random
+import math
 
 class Game():
     def __init__(self):
@@ -15,31 +18,50 @@ class Game():
         #black: 0, 0, 0 self.BLACK isn't black rn lol
         #white: 255, 255, 255 self.WHITE isn't white rn either
         self.main_menu = MainMenu(self)
-        self.options = OptionsMenu(self)
+        self.rules = RulesMenu(self)
         self.credits = CreditsMenu(self)
         self.curr_menu = self.main_menu
         #new:
         self.clock = pygame.time.Clock()
 
         pygame.mixer.music.load("locknchase\stuff\lock_n_chase_ost_stage1-1.mp3") #add mp3 file
-        pygame.mixer.music.set_volume(0.2) #make it less maybe
+        pygame.mixer.music.set_volume(0) #make it less maybe
         #pygame.mixer.music.play()
         pygame.mixer.music.play(loops=-1)
 
+        #new:
+        self.level_menu = LevelMenu(self)
+        self.selected_level = 1 #default
        
     def game_loop(self):
-        maze_game = MazeGame(self)
+        maze_game = MazeGame(self, level=self.selected_level)
+        move_delay_silly = 0
         while self.playing:
             self.check_events()
-            if self.START_KEY: #player clicks to end
+            if self.BACK_KEY: #player clicks to end
                 self.playing = False
+            if maze_game.lives <= 0: #player game over -> go back to main menu ------------------------------------game won/lost screen?
+                self.playing = False
+                self.curr_menu = self.main_menu
+                break
             #put canvas on screen
             self.display.fill(self.BLACK) #reset screen by filling it black (flipbook vibes)
+            maze_game.update_lock()
             maze_game.draw_maze()
             maze_game.draw_silly()
             maze_game.draw_player()
             maze_game.handle_input_player()
+            #dynamicaaly move silly 
+            if maze_game.level == 1:
+                if move_delay_silly % 10 == 0:  # itni movs k baad do simmulated annealing thing
+                #maze_game.handle_input_silly_level1_simulated_annealing()
+                    maze_game.step_simulated_annealing()
+                    move_delay_silly = 0
+                move_delay_silly += 1
             maze_game.check_collisions()
+            #if level == 1, then handle_silly_input_level1
+
+            #maze_game.check_collisions()
             #self.draw_text('Thanks for Playing', 20, self.DISPLAY_W/2, self.DISPLAY_H/2) #center of circle
             self.window.blit(self.display, (0, 0)) #align display with window
             pygame.display.update() #moves image onto the screen
@@ -74,8 +96,9 @@ class Game():
         self.display.blit(text_surface, text_rect)
 
 class MazeGame:
-    def __init__(self, game):
+    def __init__(self, game, level):
         self.game = game
+        self.level = level
         self.maze = [
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
@@ -132,6 +155,19 @@ class MazeGame:
         self.maze_image1 = pygame.image.load("locknchase\stuff/lv1_1lives.png")
         self.maze_image1 = pygame.transform.scale(self.maze_image1, (self.game.DISPLAY_W, self.game.DISPLAY_H))
 
+        #lock
+        self.lock_pos = None #to store lock's position
+        self.lock_timer = 0 #will remain ON 5 seconds
+        self.lock_image = pygame.image.load("locknchase/stuff/lock.png")
+        self.lock_image = pygame.transform.scale(self.lock_image, (self.cell_size, self.cell_size))
+
+
+        # -------------- simulated annealing --------------
+        self.temperature = 1.0  
+        self.cooling_factor = 0.99
+        self.temperature_min = 0.01
+        # -------------------------------------------------
+        
     def draw_maze(self):
         if self.lives == 3:
             self.game.display.blit(self.maze_image3, (0, 0))
@@ -146,7 +182,13 @@ class MazeGame:
                 y = row_index * self.cell_size + self.y_offset
                 if cell == 2: #coin
                     self.game.display.blit(self.coin_image, (x, y))        
-                    #print(f'cell printed at: {row_index}{col_index}')           
+                    #print(f'cell printed at: {row_index}{col_index}')   
+                   
+        if self.lock_pos: #if placed/is not None
+            lock_x = self.lock_pos[1] * self.cell_size + self.x_offset
+            lock_y = self.lock_pos[0] * self.cell_size + self.y_offset
+            self.game.display.blit(self.lock_image, (lock_x, lock_y))
+
         self.game.draw_text(f'Score: {self.player_score}', size=24, x=1100, y=75)
 
 
@@ -170,6 +212,19 @@ class MazeGame:
             self.move_player(0, -1)
         if keys[pygame.K_d]: #right
             self.move_player(0, 1)
+        if keys[pygame.K_q]: #q for lock pressed
+            self.place_lock()
+
+    def place_lock(self):
+        if self.lock_pos is None and self.player_old_pos != self.silly_pos:
+            self.lock_pos = self.player_old_pos[:] #shallow copy of list, separate object in memory
+            self.lock_timer = time.time() #start timer
+            print(f"lock placed at: {self.lock_pos}")
+    
+    def update_lock(self):
+        if self.lock_pos and time.time() - self.lock_timer >= 5: #5 seconds passed
+            self.lock_pos = None
+            print(f"lock removed from: {self.lock_pos}")
 
     def check_collisions(self):
         if self.player_pos == self.silly_pos:
@@ -179,43 +234,93 @@ class MazeGame:
             #now reset positions:
             self.player_pos = [15, 1]
             self.silly_pos = [1, 1]
-            if self.lives == 0: #check if lives r 0
-                print("game over")
-                self.game.playing = False
+            # if self.lives == 0: #check if lives r 0
+            #     print("game over")
+            #     self.game.playing = False
 
     def move_player(self, row_offset, col_offset):
         new_x, new_y = self.player_pos[0]+row_offset, self.player_pos[1]+col_offset
-        if self.maze[new_x][new_y] == 0:
+        if self.maze[new_x][new_y] != 1 and [new_x, new_y] != self.lock_pos: #cant go on wall and lock areas
             self.player_old_pos = self.player_pos[:] #shallow copy of list, separate object in memory
             self.player_pos = [new_x, new_y]
-        elif self.maze[new_x][new_y] == 2:
-            self.player_old_pos = self.player_pos[:] #shallow copy of list, separate object in memory
-            self.player_pos = [new_x, new_y]
-            self.player_score += 10
-            self.maze[new_x][new_y] = 0
-            self.coin_sound.play()
-            print(f'score: {self.player_score}')
-        
+            if self.maze[new_x][new_y] == 2: #check for coin
+                self.player_score += 10
+                self.maze[new_x][new_y] = 0 #remove coin from board basically
+                self.coin_sound.play()
+                print(f'score: {self.player_score}')
 
     def move_silly(self, row_offset, col_offset):
         new_x, new_y = self.silly_pos[0]+row_offset, self.silly_pos[1]+col_offset
-        if self.maze[new_x][new_y] == 0:
+        if self.maze[new_x][new_y] != 1 and [new_x, new_y] != self.lock_pos:
             self.silly_pos = [new_x, new_y]
-        elif self.maze[new_x][new_y] == 2:
-            self.silly_pos = [new_x, new_y]
-
         
-
-        pass
+    #-------------------------------- simulated annealing --------------------------------
+    def calculate_distance(self, pos1, pos2):
+        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
     
-    # def game_loop(self):
-    #     while self.game.playing:
-    #         self.game.check_events()
-    #         self.handle_input_player()
-    #         self.game.display.fill((0, 0, 0)) #cls
-    #         self.draw_maze()
-    #         self.draw_player()
-    #         self.draw_silly()
-    #         pygame.display.update()
-    #         #self.clock.tick(6) #control frame rate
-    #         self.game.reset_keys()
+    def get_random_neighbor(self, current_pos):
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  
+        random.shuffle(directions) #generate koi bhi up down left right random movement for silly
+        possible_positions = []
+        for direction in directions:
+            neighbor = [current_pos[0] + direction[0], current_pos[1] + direction[1]]
+            if 0 <= neighbor[0] < len(self.maze) and 0 <= neighbor[1] < len(self.maze[0]):
+                 # here i added check k do not overlap silly position with player cz simmulated annealing do be OVERPERFRMING
+                #if self.maze[neighbor[0]][neighbor[1]] != 1 and neighbor != self.player_pos: 
+                if self.maze[neighbor[0]][neighbor[1]] != 1 :
+                   # print(neighbor)
+                    possible_positions.append(neighbor)
+        return random.choice(possible_positions) if possible_positions else None
+
+
+    def step_simulated_annealing(self):
+        if self.temperature <= self.temperature_min:
+            return  # Exit if the temperature has cooled down enough
+
+        current_pos = self.silly_pos
+        current_cost = self.calculate_distance(current_pos, self.player_pos)
+
+        neighbor = self.get_random_neighbor(current_pos)
+        if neighbor is None:
+            return  # Skip if no valid moves
+
+        neighbor_cost = self.calculate_distance(neighbor, self.player_pos)
+        if neighbor_cost < current_cost:
+            self.silly_pos = neighbor  # Move to better position
+        else:
+            change_in_energy = neighbor_cost - current_cost
+            if math.exp(-change_in_energy / self.temperature) > random.random():
+                self.silly_pos = neighbor  # Move to worse position probabilistically
+
+        self.temperature *= self.cooling_factor  # Cool down
+
+
+    def handle_input_silly_level1_simulated_annealing(self):
+        
+        current_pos = self.silly_pos
+        print("current silly pos "+str(current_pos))
+        current_cost = self.calculate_distance(current_pos, self.player_pos)
+        print("current silly pos "+str(current_cost))
+        temperature = 1.0
+        temperature_min = 0.01
+        cooling_factor = 0.99
+
+        while temperature > temperature_min:
+            neighbor = self.get_random_neighbor(current_pos)
+            if neighbor is None:
+                continue
+            neighbor_cost = self.calculate_distance(neighbor, self.player_pos)
+
+            if neighbor_cost < current_cost:
+                current_pos = neighbor
+                current_cost = neighbor_cost
+            else:
+                change_in_energy = neighbor_cost - current_cost
+                if math.exp(-change_in_energy / temperature) > random.random():
+                    current_pos = neighbor
+                    current_cost = neighbor_cost
+
+            temperature *= cooling_factor
+        
+        self.silly_pos = current_pos
+    #-------------------------------------------------------------------------------------
--- menu.py
+++ menu.py
@@ -9,7 +9,7 @@ class Menu():
         self.offset = 150 #so it's on the left of our text --change----------------------------------------------------------------------
     
     def draw_cursor(self):
-        self.game.draw_text('*', 25, self.cursor_rect.x, self.cursor_rect.y)
+        self.game.draw_text('*', 25, self.cursor_rect.x+80, self.cursor_rect.y)
 
     def blit_screen(self):
         self.game.window.blit(self.game.display, (0, 0)) #align display with window
@@ -21,7 +21,7 @@ class MainMenu(Menu): #inherited Menu class
         Menu.__init__(self, game)
         self.state = "Start" #point cursor at start
         self.startx, self.starty = self.mid_w, self.mid_h-55 #move height a bit down in the screen
-        self.optionsx, self.optionsy = self.mid_w, self.mid_h-5 #move height a bit down in the screen
+        self.rulesx, self.rulesy = self.mid_w, self.mid_h-5 #move height a bit down in the screen
         self.creditsx, self.creditsy = self.mid_w, self.mid_h+45 #move height a bit down in the screen
         self.cursor_rect.midtop = (self.startx-self.offset, self.starty+4) #starting position of our cursor
         self.background_image = pygame.image.load("locknchase\stuff/bg_game.png")
@@ -34,8 +34,8 @@ class MainMenu(Menu): #inherited Menu class
             self.check_input()
             self.game.display.fill(self.game.BLACK)
             self.game.display.blit(self.background_image, (0, 0))
-            self.game.draw_text("Start Game", 35, self.startx, self.starty)
-            self.game.draw_text("Options", 35, self.optionsx, self.optionsy)
+            self.game.draw_text("Play", 35, self.startx, self.starty)
+            self.game.draw_text("Rules", 35, self.rulesx, self.rulesy)
             self.game.draw_text("Credits", 35, self.creditsx, self.creditsy)
             self.draw_cursor()
             self.blit_screen()
@@ -43,37 +43,38 @@ class MainMenu(Menu): #inherited Menu class
     def move_cursor(self):
         if self.game.DOWN_KEY:
             if self.state == 'Start':
-                self.cursor_rect.midtop = (self.optionsx-self.offset+45, self.optionsy+4)
-                self.state = 'Options'
-            elif self.state == 'Options':
-                self.cursor_rect.midtop = (self.creditsx-self.offset+45, self.creditsy+4)
+                self.cursor_rect.midtop = (self.rulesx-self.offset-45, self.rulesy+4)
+                self.state = 'Rules'
+            elif self.state == 'Rules':
+                self.cursor_rect.midtop = (self.creditsx-self.offset-45, self.creditsy+4)
                 self.state = 'Credits'
             elif self.state == 'Credits':
                 self.cursor_rect.midtop = (self.startx-self.offset, self.starty+4)
                 self.state = 'Start'
         elif self.game.UP_KEY:
             if self.state == 'Start':
-                self.cursor_rect.midtop = (self.creditsx-self.offset+45, self.creditsy+4)
+                self.cursor_rect.midtop = (self.creditsx-self.offset-45, self.creditsy+4)
                 self.state = 'Credits'
-            elif self.state == 'Options':
+            elif self.state == 'Rules':
                 self.cursor_rect.midtop = (self.startx-self.offset, self.starty+4)
                 self.state = 'Start'
             elif self.state == 'Credits':
-                self.cursor_rect.midtop = (self.optionsx-self.offset+45, self.optionsy+4)
-                self.state = 'Options'
+                self.cursor_rect.midtop = (self.rulesx-self.offset-45, self.rulesy+4)
+                self.state = 'Rules'
             
     def check_input(self):
         self.move_cursor() #check if user wanted to move the cursor
         if self.game.START_KEY:
             if self.state == 'Start':
-                self.game.playing = True
-            elif self.state == 'Options':
-                self.game.curr_menu = self.game.options
+                #self.game.playing = True
+                self.game.curr_menu = self.game.level_menu
+            elif self.state == 'Rules':
+                self.game.curr_menu = self.game.rules
             elif self.state == 'Credits':
                 self.game.curr_menu = self.game.credits
             self.run_display = False #to make main menu stop displaying
 
-class OptionsMenu(Menu): #inherited Menu class
+class RulesMenu(Menu): #inherited Menu class
     def __init__(self, game):
         Menu.__init__(self, game)
         self.state = 'Volume'
@@ -87,7 +88,7 @@ class OptionsMenu(Menu): #inherited Menu class
             self.game.check_events()
             self.check_input()
             self.game.display.fill(self.game.BLACK)
-            self.game.draw_text("Options", 20, self.game.DISPLAY_W/2, self.game.DISPLAY_H/2 - 30)
+            self.game.draw_text("Rules", 20, self.game.DISPLAY_W/2, self.game.DISPLAY_H/2 - 30)
             self.game.draw_text("Volume", 15, self.volx, self.voly)
             self.game.draw_text("Controls", 15, self.controlsx, self.controlsy)
             self.draw_cursor()
@@ -124,3 +125,64 @@ class CreditsMenu(Menu): #inherited Menu class
             self.game.display.blit(self.credits_image, (0, 0))
             self.blit_screen() #sets all flags back to false and displays screen
 
+class LevelMenu(Menu): #inherited Menu class
+    def __init__(self, game):
+        super().__init__(game) #same as Menu.__init__(self, game), but better actually
+        self.state = "Level 1"
+        self.level1x, self.level1y = self.mid_w, self.mid_h - 30
+        self.level2x, self.level2y = self.mid_w, self.mid_h + 10
+        self.level3x, self.level3y = self.mid_w, self.mid_h + 50
+        self.cursor_rect.midtop = (self.level1x - self.offset, self.level1y + 4)
+
+    def display_menu(self):
+        self.run_display = True
+        while self.run_display:
+            self.game.check_events()
+            self.check_input()
+            self.game.display.fill(self.game.BLACK)
+            self.game.draw_text("Select Level", 35, self.mid_w, self.mid_h - 100)
+            self.game.draw_text("Level 1", 30, self.level1x, self.level1y)
+            self.game.draw_text("Level 2", 30, self.level2x, self.level2y)
+            self.game.draw_text("Level 3", 30, self.level3x, self.level3y)
+            self.draw_cursor()
+            self.blit_screen()
+
+    def move_cursor(self):
+        if self.game.DOWN_KEY:
+            if self.state == "Level 1":
+                self.state = "Level 2"
+                self.cursor_rect.midtop = (self.level2x - self.offset, self.level2y + 4)
+            elif self.state == "Level 2":
+                self.state = "Level 3"
+                self.cursor_rect.midtop = (self.level3x - self.offset, self.level3y + 4)
+            elif self.state == "Level 3":
+                self.state = "Level 1"
+                self.cursor_rect.midtop = (self.level1x - self.offset, self.level1y + 4)
+        elif self.game.UP_KEY:
+            if self.state == "Level 1":
+                self.state = "Level 3"
+                self.cursor_rect.midtop = (self.level3x - self.offset, self.level3y + 4)
+            elif self.state == "Level 2":
+                self.state = "Level 1"
+                self.cursor_rect.midtop = (self.level1x - self.offset, self.level1y + 4)
+            elif self.state == "Level 3":
+                self.state = "Level 2"
+                self.cursor_rect.midtop = (self.level2x - self.offset, self.level2y + 4)
+        
+    def check_input(self):
+        self.move_cursor()
+        if self.game.BACK_KEY:
+            self.game.curr_menu = self.game.main_menu
+            self.run_display = False
+        elif self.game.START_KEY:
+            if self.state == "Level 1":
+                self.game.playing = True
+            elif self.state == "Level 2":
+                self.game.playing = True
+                self.game.selected_level = 2
+            elif self.state == "Level 3":
+                self.game.playing = True
+                self.game.selected_level = 3
+            self.run_display = False
+
+            
