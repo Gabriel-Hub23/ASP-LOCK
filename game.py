96bd5b7ace7c22beb6bddce0db40151fa8e285f8
Maha <75489692+mahaqj@users.noreply.github.com>
2024-12-08 04:11:09 +0500

    Add files via upload

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import time
 import random
 import math
 import heapq
+import copy 
 
 class Game():
     def __init__(self):
@@ -13,7 +14,7 @@ class Game():
         self.DISPLAY_W, self.DISPLAY_H = 1280, 720
         self.display = pygame.Surface((self.DISPLAY_W, self.DISPLAY_H))
         self.window = pygame.display.set_mode(((self.DISPLAY_W, self.DISPLAY_H)))
-        self.font_name = "locknchase\stuff/NTBrickSans.ttf"
+        self.font_name = "locknchase\\stuff\\NTBrickSans.ttf"
         self.BLACK, self.WHITE = (245, 221, 203), (196, 125, 18)
         #blue: 197, 219, 237
         #black: 0, 0, 0 self.BLACK isn't black rn lol
@@ -21,12 +22,14 @@ class Game():
         self.main_menu = MainMenu(self)
         self.rules = RulesMenu(self)
         self.credits = CreditsMenu(self)
+        self.gameover_menu = GameOverMenu(self)
+        self.gamewon_menu = GameWonMenu(self)
         self.curr_menu = self.main_menu
         #new:
         self.clock = pygame.time.Clock()
 
-        pygame.mixer.music.load("locknchase\stuff\lock_n_chase_ost_stage1-1.mp3") #add mp3 file
-        pygame.mixer.music.set_volume(0) #make it less maybe
+        pygame.mixer.music.load("locknchase\\stuff\\lock_n_chase_ost_stage1-1.mp3") #add mp3 file
+        pygame.mixer.music.set_volume(0.1) #make it less maybe
         #pygame.mixer.music.play()
         pygame.mixer.music.play(loops=-1)
 
@@ -38,16 +41,22 @@ class Game():
         maze_game = MazeGame(self, level=self.selected_level)
         player_move_timer = 0
         silly_move_timer = 0
-        player_delay = 5
-        silly_delay = 10
+        player_delay = 5+0
+        silly_delay = 5+5
 
         while self.playing:
             self.check_events()
             if self.BACK_KEY:
                 self.playing = False
             if maze_game.lives <= 0:
+                maze_game.gameover_sfx.play()
                 self.playing = False
-                self.curr_menu = self.main_menu
+                self.curr_menu = self.gameover_menu
+                break
+            if maze_game.player_score == 1480:
+                maze_game.gamewon_sfx.play()
+                self.playing = False
+                self.curr_menu = self.gamewon_menu
                 break
 
             self.display.fill(self.BLACK)
@@ -73,7 +82,13 @@ class Game():
                         next_pos = maze_game.path.pop(0)
                         if not maze_game.move_silly_locks(next_pos):
                             maze_game.path = maze_game.a_star_search(tuple(maze_game.silly_pos), tuple(maze_game.player_pos))
-
+                #LLLL
+                elif maze_game.level == 3:
+                    print("i am in level 3")
+                    move, score = maze_game.best_move(3, True)  #the first value passed here is the depth to which we explore game tree 3 is optimal i think though a bit laggy :(
+                    if move:
+                        maze_game.silly_pos = move
+                        print(f"best move {move} found with score {score}")
                 silly_move_timer = 0 
 
             player_move_timer += 1
@@ -95,9 +110,9 @@ class Game():
                     self.START_KEY = True
                 if event.key == pygame.K_BACKSPACE: #backspace clicked
                     self.BACK_KEY = True
-                if event.key == pygame.K_DOWN: #down arrow pressed
+                if event.key == pygame.K_s: #S pressed
                     self.DOWN_KEY = True
-                if event.key == pygame.K_UP: #up arrow pressed
+                if event.key == pygame.K_w: #W pressed
                     self.UP_KEY = True
 
     def reset_keys(self):
@@ -141,20 +156,25 @@ class MazeGame:
         self.player_pos = [15, 1]
         self.player_old_pos = [15, 2]
         self.player_score = 0
-        self.player_image = pygame.image.load("locknchase\stuff\lupin_colour-Photoroom.png")
+        self.player_image = pygame.image.load("locknchase\\stuff\\lupin_colour-Photoroom.png")
         self.player_image = pygame.transform.scale(self.player_image, (self.cell_size, self.cell_size))
 
         self.silly_pos = [1, 1]
-        self.silly_image = pygame.image.load("locknchase\stuff\silly_colour.png")
+        self.silly_image = pygame.image.load("locknchase\\stuff\\silly_colour.png")
         self.silly_image = pygame.transform.scale(self.silly_image, (self.cell_size, self.cell_size))
 
-        self.coin_image = pygame.image.load("locknchase\stuff\coin_colour.png")
+        self.coin_image = pygame.image.load("locknchase\\stuff\\coin_colour.png")
         self.coin_image = pygame.transform.scale(self.coin_image, (self.cell_size, self.cell_size))
-        self.coin_sound = pygame.mixer.Sound("locknchase\stuff\coin_sound_effect.mp3")
+        self.coin_sound = pygame.mixer.Sound("locknchase\\stuff\\coin_sound_effect.mp3")
         self.coin_sound.set_volume(0.5)
 
-        self.life_lost_sound = pygame.mixer.Sound("locknchase\stuff/life_lost_sound.wav")
+        self.life_lost_sound = pygame.mixer.Sound("locknchase\\stuff\\life_lost_sound.wav")
         self.life_lost_sound.set_volume(0.7)
+
+        self.gamewon_sfx =  pygame.mixer.Sound("locknchase\\stuff\\game_won_sfx.wav")
+        self.life_lost_sound.set_volume(0.3)
+        self.gameover_sfx = pygame.mixer.Sound("locknchase\\stuff\\game_over_sfx.wav")
+        self.life_lost_sound.set_volume(0.3)
 
         #dimensions and offsets of maze (will use these to place in centre)
         self.maze_width = len(self.maze[0]) * self.cell_size
@@ -163,17 +183,31 @@ class MazeGame:
         self.y_offset = (self.game.DISPLAY_H - self.maze_height) // 2
 
         #lvl 1 ??
-        self.maze_image3 = pygame.image.load("locknchase\stuff/lv1_3lives.png")
-        self.maze_image3 = pygame.transform.scale(self.maze_image3, (self.game.DISPLAY_W, self.game.DISPLAY_H))
-        self.maze_image2 = pygame.image.load("locknchase\stuff/lv1_2lives.png")
-        self.maze_image2 = pygame.transform.scale(self.maze_image2, (self.game.DISPLAY_W, self.game.DISPLAY_H))
-        self.maze_image1 = pygame.image.load("locknchase\stuff/lv1_1lives.png")
-        self.maze_image1 = pygame.transform.scale(self.maze_image1, (self.game.DISPLAY_W, self.game.DISPLAY_H))
+        self.maze_image3_lvl1 = pygame.image.load("locknchase\\stuff\\lv1_3lives.png")
+        self.maze_image3_lvl1 = pygame.transform.scale(self.maze_image3_lvl1, (self.game.DISPLAY_W, self.game.DISPLAY_H))
+        self.maze_image2_lvl1 = pygame.image.load("locknchase\\stuff\\lv1_2lives.png")
+        self.maze_image2_lvl1 = pygame.transform.scale(self.maze_image2_lvl1, (self.game.DISPLAY_W, self.game.DISPLAY_H))
+        self.maze_image1_lvl1 = pygame.image.load("locknchase\\stuff\\lv1_1lives.png")
+        self.maze_image1_lvl1 = pygame.transform.scale(self.maze_image1_lvl1, (self.game.DISPLAY_W, self.game.DISPLAY_H))
+        #lvl 2:
+        self.maze_image3_lvl2 = pygame.image.load("locknchase\\stuff\\lv2_3lives.png")
+        self.maze_image3_lvl2 = pygame.transform.scale(self.maze_image3_lvl2, (self.game.DISPLAY_W, self.game.DISPLAY_H))
+        self.maze_image2_lvl2 = pygame.image.load("locknchase\\stuff\\lv2_3lives.png")
+        self.maze_image2_lvl2 = pygame.transform.scale(self.maze_image2_lvl2, (self.game.DISPLAY_W, self.game.DISPLAY_H))
+        self.maze_image1_lvl2 = pygame.image.load("locknchase\\stuff\\lv2_3lives.png")
+        self.maze_image1_lvl2 = pygame.transform.scale(self.maze_image1_lvl2, (self.game.DISPLAY_W, self.game.DISPLAY_H))
+        #lvl 3:
+        self.maze_image3_lvl3 = pygame.image.load("locknchase\\stuff\\lv3_3lives.png")
+        self.maze_image3_lvl3 = pygame.transform.scale(self.maze_image3_lvl3, (self.game.DISPLAY_W, self.game.DISPLAY_H))
+        self.maze_image2_lvl3 = pygame.image.load("locknchase\\stuff\\lv3_3lives.png")
+        self.maze_image2_lvl3 = pygame.transform.scale(self.maze_image2_lvl3, (self.game.DISPLAY_W, self.game.DISPLAY_H))
+        self.maze_image1_lvl3 = pygame.image.load("locknchase\\stuff\\lv3_3lives.png")
+        self.maze_image1_lvl3 = pygame.transform.scale(self.maze_image1_lvl3, (self.game.DISPLAY_W, self.game.DISPLAY_H))
 
         #lock
         self.lock_pos = None #to store lock's position
         self.lock_timer = 0 #will remain ON 5 seconds
-        self.lock_image = pygame.image.load("locknchase/stuff/lock.png")
+        self.lock_image = pygame.image.load("locknchase\\stuff\\lock.png")
         self.lock_image = pygame.transform.scale(self.lock_image, (self.cell_size, self.cell_size))
 
         # #animation/interpolation stuff
@@ -192,14 +226,32 @@ class MazeGame:
         self.path=[] #this is the shortest path silly would take to catch lupin 
         self.player_moved = False
         self.obstacle_changed = False
+        #LLLL
+        #--------------minimax-------------------
+        self.move_history = []
         
     def draw_maze(self):
-        if self.lives == 3:
-            self.game.display.blit(self.maze_image3, (0, 0))
-        elif self.lives == 2:
-            self.game.display.blit(self.maze_image2, (0, 0))
-        elif self.lives == 1:
-            self.game.display.blit(self.maze_image1, (0, 0))
+        if self.level == 1:
+            if self.lives == 3:
+                self.game.display.blit(self.maze_image3_lvl1, (0, 0))
+            elif self.lives == 2:
+                self.game.display.blit(self.maze_image2_lvl1, (0, 0))
+            elif self.lives == 1:
+                self.game.display.blit(self.maze_image1_lvl1, (0, 0))
+        elif self.level == 2:
+            if self.lives == 3:
+                self.game.display.blit(self.maze_image3_lvl2, (0, 0))
+            elif self.lives == 2:
+                self.game.display.blit(self.maze_image2_lvl2, (0, 0))
+            elif self.lives == 1:
+                self.game.display.blit(self.maze_image1_lvl2, (0, 0))
+        elif self.level == 3:
+            if self.lives == 3:
+                self.game.display.blit(self.maze_image3_lvl3, (0, 0))
+            elif self.lives == 2:
+                self.game.display.blit(self.maze_image2_lvl3, (0, 0))
+            elif self.lives == 1:
+                self.game.display.blit(self.maze_image1_lvl3, (0, 0))
 
         for row_index, row in enumerate(self.maze):
             for col_index, cell in enumerate(row):
@@ -214,7 +266,7 @@ class MazeGame:
             lock_y = self.lock_pos[0] * self.cell_size + self.y_offset
             self.game.display.blit(self.lock_image, (lock_x, lock_y))
 
-        self.game.draw_text(f'Score: {self.player_score}', size=24, x=1100, y=75)
+        self.game.draw_text(f'Score: {self.player_score}', size=24, x=1100, y=510)
 
 
     def draw_player(self):
@@ -459,3 +511,117 @@ class MazeGame:
             self.silly_pos = list(next_pos)
             #print(f"Moving Silly to {self.silly_pos}")
             return True
+        
+    #------------------------MINIMAX ALGORITHM +ALPHA BETA PRUNING (DEPTH LIMITED-----------------
+    def evaluate_position(self): #evaluation function , returns basically a score how good a move is 
+        if self.player_pos == self.silly_pos: #a v v v large amazing value if it leads to catching lupin
+            return float('inf') 
+        else:#find manhattan distance bw lupin and silly
+            distance_to_lupin = abs(self.silly_pos[0] - self.player_pos[0]) + abs(self.silly_pos[1] - self.player_pos[1])
+            # we are also keeping track of coins collected aka the player score towards evaluation 
+            #returning a negative value cz silly is minimizing the distance , and overall maximizing the evaluation score
+            #jinta silly is close to lupin  utna better score 
+            return -distance_to_lupin - (0.5 * self.player_score)
+    
+    def get_all_possible_moves(self):
+        possible_moves = []
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  
+        for direction in directions:
+            new_pos = [self.silly_pos[0] + direction[0], self.silly_pos[1] + direction[1]] 
+            #ake sure that when generating moves hum khyaal rakhen of walls and locks
+            if 0 <= new_pos[0] < len(self.maze) and 0 <= new_pos[1] < len(self.maze[0]) and self.maze[new_pos[0]][new_pos[1]] != 1 and (self.lock_pos is None or tuple(new_pos) != tuple(self.lock_pos)):
+                possible_moves.append(new_pos)
+        return possible_moves 
+    def minimax(self, depth, alpha, beta, is_maximizing):
+        if depth == 0 or self.game_over():
+            return self.evaluate_position()
+        if is_maximizing:#AI tuern aka SILLY turn 
+            max_eval = float('-inf')
+            for move in self.get_all_possible_moves():
+                # we cant simulate moves on roiginal game cz we have not yet decided best move yet
+                #hence creating a clone of current game state , later well restore the game state too
+                new_game = self.clone_state()
+                new_game.silly_pos = move #simulate move on silly
+                #create all counter moves of human is_maximizing=False
+                eval = new_game.minimax(depth - 1, alpha, beta, False)
+                #keep track of best move saath saaath , like update if a behter move found at ebery iteration
+                max_eval = max(max_eval, eval)
+                #alpha picks the larger value move everytime
+                alpha = max(alpha, eval)
+                if beta <= alpha: #pruning condition , iss sey neechey tree explore karney ki zarooorat hi nahi , cz jitna acha score aa chuka iss sey kam hi aaye ga , iss sey zyaada aa hi nahi sakta
+                    print("Pruning game tree chop chop")
+                    break
+            return max_eval
+        else: #is_maximizing=False   ###its Human Player (lupin's) turn
+            min_eval = float('inf')
+            for move in self.get_all_possible_moves():
+                new_game = self.clone_state()
+                new_game.silly_pos = move
+                #human k liye counter AI moves check and pick best move human could make (hehe then later on well counter this human move too in best move function)
+                eval = new_game.minimax(depth - 1, alpha, beta, True) #True for AI move 
+                min_eval = min(min_eval, eval)
+                #beta always picks the lower value side
+                beta = min(beta, eval)
+                if alpha >= beta:
+                    print("Pruning game tree in minimizing chop chop")
+                    break
+            return min_eval
+   
+    def best_move(self, depth, is_maximizing):
+        alpha= float('-inf') #next alpha(max value) iss sey to kam hi ho ga
+        beta= float('inf') #next beta (min vlaue) iss sey to zyaada hi ho gi 
+        best_score = float('-inf') if is_maximizing else float('inf')
+        best_move = None
+ #aik dafa run is_maximizing code yahan bhi , this actually like initiates the gae tree ka root , yahan sey neechey game tree generate hota hey
+        for move in self.get_all_possible_moves():
+            cloned_game = self.clone_state()
+            cloned_game.silly_pos = move
+            score = cloned_game.minimax(depth - 1, alpha, beta, not is_maximizing)
+            #agar this move is already in the move history 
+            #penalize it by reducing its score -1 so that baar baar same move na li jaaye
+            #this solves the oscillation problem  silly moving back and forth in maze cz of similar scores
+            if move in self.move_history[-3:]:  #last 3 moves look at 
+                score -= 1  
+
+            if is_maximizing and score > best_score:
+                #keeping yrack of and updating the best score and best move all same as we did in tic tac toe too
+                best_score = score
+                best_move = move
+                alpha = max(alpha, best_score)
+                print(f"updating  best move found: {best_move} with score {best_score}")
+            elif not is_maximizing and score < best_score:
+                best_score = score
+                best_move = move
+                beta = min(beta, best_score)
+            if alpha >= beta:
+                print("pruning at best move function")
+                break
+
+        self.move_history.append(best_move) #update the move_hisotry too na
+        if len(self.move_history) > 10:  #pop form head if its getting larger than a certain mempory limit ie 10
+            self.move_history.pop(0)
+        return best_move, best_score
+    def game_over(self):#winning/losing conditions
+        return self.lives <= 0 or self.player_pos == self.silly_pos #or self.player_score>=1480
+    def clone_state(self):
+        #could not use deep copy function of copy library cz it was not copying surfaces and stuff of pygame library
+        cloned_game = MazeGame(self.game, self.level)  
+        #create copy for only those relevant attributes we could need
+        cloned_game.maze = [row[:] for row in self.maze]  
+        cloned_game.cell_size = self.cell_size
+
+        cloned_game.lives = self.lives
+        cloned_game.player_pos = self.player_pos[:]
+        cloned_game.player_old_pos = self.player_old_pos[:]
+        cloned_game.player_score = self.player_score
+
+        cloned_game.silly_pos = self.silly_pos[:]
+        
+        cloned_game.lock_pos = None if self.lock_pos is None else self.lock_pos[:]
+        cloned_game.lock_timer = self.lock_timer
+
+        
+        return cloned_game
+
+
+    
