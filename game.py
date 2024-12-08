d60a2664e5c5ee71f23b6de11a51b057fded8b81
Gabriel-Hub23 <123456+Gabriel-Hub23@users.noreply.github.com>
2024-12-08 14:48:59 +0500

    final ver.
    code cleaned

--- game.py
+++ game.py
@@ -15,25 +15,20 @@ class Game():
         self.display = pygame.Surface((self.DISPLAY_W, self.DISPLAY_H))
         self.window = pygame.display.set_mode(((self.DISPLAY_W, self.DISPLAY_H)))
         self.font_name = "locknchase\\stuff\\NTBrickSans.ttf"
-        self.BLACK, self.WHITE = (245, 221, 203), (196, 125, 18)
-        #blue: 197, 219, 237
-        #black: 0, 0, 0 self.BLACK isn't black rn lol
-        #white: 255, 255, 255 self.WHITE isn't white rn either
+        self.screen_colour = (245, 221, 203)
         self.main_menu = MainMenu(self)
         self.rules = RulesMenu(self)
         self.credits = CreditsMenu(self)
         self.gameover_menu = GameOverMenu(self)
         self.gamewon_menu = GameWonMenu(self)
         self.curr_menu = self.main_menu
-        #new:
+        
         self.clock = pygame.time.Clock()
 
         pygame.mixer.music.load("locknchase\\stuff\\lock_n_chase_ost_stage1-1.mp3") #add mp3 file
-        pygame.mixer.music.set_volume(0.1) #make it less maybe
-        #pygame.mixer.music.play()
-        pygame.mixer.music.play(loops=-1)
+        pygame.mixer.music.set_volume(0.1)
+        pygame.mixer.music.play(loops=-1) #-1 means run indefinitely
 
-        #new:
         self.level_menu = LevelMenu(self)
         self.selected_level = 1 #default
     
@@ -41,8 +36,8 @@ class Game():
         maze_game = MazeGame(self, level=self.selected_level)
         player_move_timer = 0
         silly_move_timer = 0
-        player_delay = 5+0
-        silly_delay = 5+5
+        player_delay = 5+0+1.5
+        silly_delay = 5+5+1.5
 
         while self.playing:
             self.check_events()
@@ -59,7 +54,7 @@ class Game():
                 self.curr_menu = self.gamewon_menu
                 break
 
-            self.display.fill(self.BLACK)
+            self.display.fill(self.screen_colour)
 
             maze_game.update_lock()
             maze_game.draw_maze()
@@ -82,10 +77,11 @@ class Game():
                         next_pos = maze_game.path.pop(0)
                         if not maze_game.move_silly_locks(next_pos):
                             maze_game.path = maze_game.a_star_search(tuple(maze_game.silly_pos), tuple(maze_game.player_pos))
-                #LLLL
+                
                 elif maze_game.level == 3:
                     print("i am in level 3")
-                    move, score = maze_game.best_move(3, True)  #the first value passed here is the depth to which we explore game tree 3 is optimal i think though a bit laggy :(
+                    move, score = maze_game.best_move(3, True) #the first value passed here is the depth to which we explore game tree 3 is optimal
+                                                               #i think though a bit laggy :(
                     if move:
                         maze_game.silly_pos = move
                         print(f"best move {move} found with score {score}")
@@ -148,8 +144,6 @@ class MazeGame:
             [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         ]
-        #new:
-        #self.clock = pygame.time.Clock()
         self.cell_size = 36
 
         self.lives = 3
@@ -182,7 +176,7 @@ class MazeGame:
         self.x_offset = (self.game.DISPLAY_W - self.maze_width) // 2
         self.y_offset = (self.game.DISPLAY_H - self.maze_height) // 2
 
-        #lvl 1 ??
+        #lvl 1:
         self.maze_image3_lvl1 = pygame.image.load("locknchase\\stuff\\lv1_3lives.png")
         self.maze_image3_lvl1 = pygame.transform.scale(self.maze_image3_lvl1, (self.game.DISPLAY_W, self.game.DISPLAY_H))
         self.maze_image2_lvl1 = pygame.image.load("locknchase\\stuff\\lv1_2lives.png")
@@ -204,30 +198,21 @@ class MazeGame:
         self.maze_image1_lvl3 = pygame.image.load("locknchase\\stuff\\lv3_3lives.png")
         self.maze_image1_lvl3 = pygame.transform.scale(self.maze_image1_lvl3, (self.game.DISPLAY_W, self.game.DISPLAY_H))
 
-        #lock
+        #lock things:
         self.lock_pos = None #to store lock's position
         self.lock_timer = 0 #will remain ON 5 seconds
         self.lock_image = pygame.image.load("locknchase\\stuff\\lock.png")
         self.lock_image = pygame.transform.scale(self.lock_image, (self.cell_size, self.cell_size))
 
-        # #animation/interpolation stuff
-        # self.animation_speed = 10  #adjust for smoothness (as needed)
-        # #self.player_animation_progress = 0.0
-        # #self.player_target_pos = self.player_pos[:]
-        # self.silly_animation_progress = 0.0
-        # self.silly_target_pos = self.silly_pos[:]
-        # self.silly_old_pos = self.silly_pos[:]
-
         # -------------- simulated annealing --------------
         self.temperature = 50.0 # now ive set it to a high value bcz i dont know how long the game is gonna keep runnning so i need a large value that would kind of sustain till end moves  
         self.cooling_factor = 0.99
         self.temperature_min = 0.01
-        #-------------a_star+csp----------------------------
+        # ------------------- a* + csp --------------------
         self.path=[] #this is the shortest path silly would take to catch lupin 
         self.player_moved = False
         self.obstacle_changed = False
-        #LLLL
-        #--------------minimax-------------------
+        # --------- minimax + alpha beta pruning ----------
         self.move_history = []
         
     def draw_maze(self):
@@ -279,20 +264,6 @@ class MazeGame:
         self.game.display.blit(self.silly_image, (x, y))
         #print('silly printed')
 
-    # def draw_silly(self):
-    #     x = self.silly_old_pos[1] * self.cell_size + self.x_offset
-    #     y = self.silly_old_pos[0] * self.cell_size + self.y_offset
-    #     target_x = self.silly_target_pos[1] * self.cell_size + self.x_offset
-    #     target_y = self.silly_target_pos[0] * self.cell_size + self.y_offset
-
-    #     interpolated_x = x + (target_x - x) * self.silly_animation_progress
-    #     interpolated_y = y + (target_y - y) * self.silly_animation_progress
-    #     self.game.display.blit(self.silly_image, (interpolated_x, interpolated_y))
-
-    #     if self.silly_animation_progress < 1.0:
-    #         self.silly_animation_progress += 1.0 / self.animation_speed
-
-
     def handle_input_player(self):
         keys = pygame.key.get_pressed() #this function actually returns a series of boolean values xD
         if keys[pygame.K_w]: #up
@@ -331,20 +302,6 @@ class MazeGame:
             self.player_pos = [15, 1]
             self.silly_pos = [1, 1]
             self.path=None
-            # if self.lives == 0: #check if lives r 0
-            #     print("game over")
-            #     self.game.playing = False
-
-    # def move_player(self, row_offset, col_offset):
-    #     new_x, new_y = self.player_pos[0]+row_offset, self.player_pos[1]+col_offset
-    #     if self.maze[new_x][new_y] != 1 and [new_x, new_y] != self.lock_pos: #cant go on wall and lock areas
-    #         self.player_old_pos = self.player_pos[:] #shallow copy of list, separate object in memory
-    #         self.player_pos = [new_x, new_y]
-    #         if self.maze[new_x][new_y] == 2: #check for coin
-    #             self.player_score += 10
-    #             self.maze[new_x][new_y] = 0 #remove coin from board basically
-    #             self.coin_sound.play()
-    #             print(f'score: {self.player_score}')
 
     def move_silly(self, row_offset, col_offset):
         new_x, new_y = self.silly_pos[0]+row_offset, self.silly_pos[1]+col_offset
@@ -363,19 +320,11 @@ class MazeGame:
                 self.maze[new_x][new_y] = 0
                 self.coin_sound.play()
 
-    # def move_silly(self, row_offset, col_offset):
-    #     new_x, new_y = self.silly_pos[0] + row_offset, self.silly_pos[1] + col_offset
-    #     if self.maze[new_x][new_y] != 1 and [new_x, new_y] != self.lock_pos:
-    #         self.silly_old_pos = self.silly_pos[:]
-    #         self.silly_pos = [new_x, new_y]
-    #         self.silly_target_pos = [new_x, new_y]
-    #         self.silly_animation_progress = 0.0
+    # --------------------------------- simulated annealing ----------------------------------
 
-   #-------------------------------- simulated annealing --------------------------------
     def calculate_distance(self, pos1, pos2):
         return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
     
-    #NNNNN
     def get_random_neighbor(self, current_pos):
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  
         random.shuffle(directions) #generate koi bhi up down left right random movement for silly
@@ -383,15 +332,14 @@ class MazeGame:
         for direction in directions:
             neighbor = [current_pos[0] + direction[0], current_pos[1] + direction[1]]
             if 0 <= neighbor[0] < len(self.maze) and 0 <= neighbor[1] < len(self.maze[0]):
-                 # here i added check k do not overlap silly position with player cz simmulated annealing do be OVERPERFORMING
-                #if self.maze[neighbor[0]][neighbor[1]] != 1 and neighbor != self.player_pos:
-                # #the current pos is silly position 
+                # here i added check k do not overlap silly position with player cz simmulated annealing do be OVERPERFORMING
+                # the current pos is silly position 
                 # self is lupin 
                 if self.maze[neighbor[0]][neighbor[1]] != 1 and (self.lock_pos is None or not self.is_one_step_away((neighbor[0], neighbor[1]), self.lock_pos)):
-                   # print(neighbor)
+                    # print(neighbor)
                     possible_positions.append(neighbor)
         return random.choice(possible_positions) if possible_positions else None
-#NNNNN
+
     def is_one_step_away(self, pos, lock_pos): #lock is one step away from playa
         if lock_pos is None:
             return False
@@ -400,13 +348,13 @@ class MazeGame:
         print("lock pos"+str(lock_pos))
         print("silly pos" +str(pos))
         
-    # One step away means either x_diff is 1 and y_diff is 0, or y_diff is 1 and x_diff is 0
+        # One step away means either x_diff is 1 and y_diff is 0, or y_diff is 1 and x_diff is 0
         if (x_diff == 1 and y_diff == 0) or (y_diff == 1 and x_diff == 0):
             print("lock one step away detected ")
             return True
         else:
             return False
-#NNNNN
+
     def handle_input_silly_level1_simulated_annealing(self):
         if self.temperature <= self.temperature_min:
             return  
@@ -427,8 +375,8 @@ class MazeGame:
                 self.silly_pos = neighbor  
 
         self.temperature *= self.cooling_factor  
-        #NNNNN
-    #------------------------Astar+CSP-----------------------------------------------------------
+        
+    # -------------------------------------- A* + CSP ----------------------------------------
 
     def a_star_search(self, start, goal):
         frontier = []
@@ -468,51 +416,36 @@ class MazeGame:
 
     def is_blocked(self, pos):
         x, y = pos
-        #if self.maze[x][y] == 1 or (self.lock_pos is not None and self.is_one_step_away(pos, self.lock_pos)):
         if self.maze[x][y] == 1 :
             return True
             #print("walls or lock heyyyy")
-        #if self.maze[x][y] == 1 :
         if self.lock_pos is not None and tuple(pos) == tuple(self.lock_pos):
             return True
         return False
 
-    # def reconstruct_path(self, came_from, start, goal):
-    #     current = goal
-    #     path = []
-    #     while current != start:
-    #         path.append(current)
-    #         current = came_from[current]
-    #     path.reverse() #cz bactracking hum ney goal sey sillly tak ki hey
-    #     #we need path from silly to goal
-    #     return path
     def reconstruct_path(self, came_from, start, goal):
         current = tuple(goal)
         path = []
         while current != tuple(start):
             if current in came_from:
-                path.append(list(current))  # Convert tuple back to list for consistency in your game
+                path.append(list(current))  #Convert tuple back to list for consistency in game
                 current = came_from[current]
             else:
-                # Handle the case where the path is incomplete or the goal/start is blocked
-                print(f"Path reconstruction failed: {current} not reachable from {start}")
-                return []  # Return an empty path or handle as needed
+                #handle the case where the path is incomplete or the goal/start is blocked
+                print(f"path reconstruction failed: {current} not reachable from {start}")
+                return []  #return an empty path
         path.reverse()
         return path
 
     def move_silly_locks(self, next_pos):
         if self.is_blocked(next_pos):
-            #print(f"Blocked: Cannot move to {next_pos}")
             return False
-        #if self.is_one_step_away(next_pos,self.lock_pos):
-            #print(f"Blocked die to lock placed : Cannot move to {next_pos}")
-         #   return False
         else:
             self.silly_pos = list(next_pos)
-            #print(f"Moving Silly to {self.silly_pos}")
             return True
-        
-    #------------------------MINIMAX ALGORITHM +ALPHA BETA PRUNING (DEPTH LIMITED-----------------
+    
+    # ------------------- minimax + alpha beta pruning (depth limited) -----------------------
+
     def evaluate_position(self): #evaluation function , returns basically a score how good a move is 
         if self.player_pos == self.silly_pos: #a v v v large amazing value if it leads to catching lupin
             return float('inf') 
@@ -572,7 +505,7 @@ class MazeGame:
         beta= float('inf') #next beta (min vlaue) iss sey to zyaada hi ho gi 
         best_score = float('-inf') if is_maximizing else float('inf')
         best_move = None
- #aik dafa run is_maximizing code yahan bhi , this actually like initiates the gae tree ka root , yahan sey neechey game tree generate hota hey
+        #aik dafa run is_maximizing code yahan bhi , this actually like initiates the gae tree ka root , yahan sey neechey game tree generate hota hey
         for move in self.get_all_possible_moves():
             cloned_game = self.clone_state()
             cloned_game.silly_pos = move
@@ -620,8 +553,6 @@ class MazeGame:
         cloned_game.lock_pos = None if self.lock_pos is None else self.lock_pos[:]
         cloned_game.lock_timer = self.lock_timer
 
-        
         return cloned_game
-
-
     
+# happy gaming! :D
